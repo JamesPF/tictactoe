685b58a5ce901ca0490b18db649dd00b0aa6742e
James Felz <James@Jamess-MacBook-Air.local>
2014-10-15 14:26:46 -0700

    next step add win logic

--- app/main.js
+++ app/main.js
@@ -3,29 +3,81 @@ var myTicTacToe = angular.module('myTicTacToe', []);
 myTicTacToe.controller('GameController', function ($scope) {
 
     $scope.gameBoard = [
-        {name: "c0"},
-        {name: "c1"},
-        {name: "c2"},
+        {status: "", name: "c0", position:[0]},
+        {status: "", name: "c1", position:[1]},
+        {status: "", name: "c2", position:[2]},
 
-        {name: "c3"},
-        {name: "c4"},
-        {name: "c5"},
+        {status: "", name: "c3", position:[3]},
+        {status: "", name: "c4", position:[4]},
+        {status: "", name: "c5", position:[5]},
 
-        {name: "c6"},
-        {name: "c7"},
-        {name: "c8"},
+        {status: "", name: "c6", position:[6]},
+        {status: "", name: "c7", position:[7]},
+        {status: "", name: "c8", position:[8]},
     ];
 
+    $scope.movecounter = 0;
+    gameover = false;
+
+    $scope.playerPicks = function(cell) {
+    $scope.movecounter = $scope.movecounter + 1 ;
+        console.log("Cell was: " + cell.status) ;
+        if (($scope.movecounter % 2) == 1) {
+            cell.status = "X" ; 
+        } else {
+            cell.status = "O" ;
+        } 
+        console.log("Cell is now: " + cell.status) ;
+        // if (gameover == false) evaluateWin();
+    
+
+    
 
 
-    // $scope.playerPicks = function(thisCell) {
-    // $scope.movecounter = $scope.movecounter + 1 ;
-    //     console.log("Cell was: " + thisCell.name) ;
-    //     if (($scope.movecounter % 2) == 1) {
-    //         thisCell.name = "X" ;  
-    //     } else {
-    //         thisCell.name = "O" ;
-    //     } 
-    //     console.log("Cell is now: " + thisCell.name) ;
-    // } ;
+    var evaluateWin = function() {
+        if ($scope.gameBoard[0] == "X" && $scope.gameBoard[1] == "X" && $scope.gameBoard[2] == "X") {
+            console.log("x");
+        } else if ($scope.gameBoard[3] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[5] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[6] == "X" && $scope.gameBoard[7] == "X" && $scope.gameBoard[8] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[0] == "X" && $scope.gameBoard[3] == "X" && $scope.gameBoard[6] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[1] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[7] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[2] == "X" && $scope.gameBoard[5] == "X" && $scope.gameBoard[8] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[0] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[8] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[2] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[6] == "X") {
+            Xwin();
+        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[1] == "O" && $scope.gameBoard[2] == "O") {
+            owin();
+        } else if ($scope.gameBoard[3] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[5] == "O") {
+            owin();
+        } else if ($scope.gameBoard[6] == "O" && $scope.gameBoard[7] == "O" && $scope.gameBoard[8] == "O") {
+            owin();
+        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[3] == "O" && $scope.gameBoard[6] == "O") {
+            owin();
+        } else if ($scope.gameBoard[1] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[7] == "O") {
+            owin();
+        } else if ($scope.gameBoard[2] == "O" && $scope.gameBoard[5] == "O" && $scope.gameBoard[8] == "O") {
+            owin();
+        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[8] == "O") {
+            owin();
+        } else if ($scope.gameBoard[2] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[6] == "O") {
+            owin();
+        } else if (movecounter == 9) {
+    //         var messageboX = document.getElementById('message');
+    //         $scope.leftMessage = "draw...";
+    //         $scope.rightMessage = "draw...";
+               console.log("The game is a tie");
+        } else {
+            movecounter += 1;
+        }
+    }
+    };
+    
+
+
 });
