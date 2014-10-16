a8d172beba97ec7661186ffb3a645ebdb324e865
James Felz <James@Jamess-MacBook-Air.local>
2014-10-16 15:01:53 -0700

    win logic almost done

--- app/main.js
+++ app/main.js
@@ -1,81 +1,90 @@
-var myTicTacToe = angular.module('myTicTacToe', []);
+var myTicTacToe = angular.module('myTicTacToe', ["firebase"]);
 
-myTicTacToe.controller('GameController', function ($scope) {
+myTicTacToe.controller('GameController', function ($scope,$firebase) {
+
+    $scope.remoteGameContainer = 
+      $firebase(new Firebase("https://mario-kong.firebaseIO.com/databaseGameContainer")) ;
+
+    $scope.moveCounter = 0 ;
 
     $scope.gameBoard = [
-        {status: "", name: "c0", position:[0]},
-        {status: "", name: "c1", position:[1]},
-        {status: "", name: "c2", position:[2]},
+        {status: "A", position: 0},
+        {status: "B", position: 1},
+        {status: "C", position: 2},
 
-        {status: "", name: "c3", position:[3]},
-        {status: "", name: "c4", position:[4]},
-        {status: "", name: "c5", position:[5]},
+        {status: "D", position: 3},
+        {status: "E", position: 4},
+        {status: "F", position: 5},
 
-        {status: "", name: "c6", position:[6]},
-        {status: "", name: "c7", position:[7]},
-        {status: "", name: "c8", position:[8]},
+        {status: "G", position: 6},
+        {status: "H", position: 7},
+        {status: "I", position: 8}
     ];
 
-    $scope.movecounter = 0;
-    gameover = false;
+    // This container object is what gets synced:
+  $scope.gameContainer = {
+    gameBoardArray: $scope.gameBoard,
+    clickCounter: $scope.moveCounter
+  } ;
 
-    $scope.playerPicks = function(cell) {
-    $scope.movecounter = $scope.movecounter + 1 ;
-        console.log("Cell was: " + cell.status) ;
-        if (($scope.movecounter % 2) == 1) {
-            cell.status = "X" ; 
+  // Everywhere else in your program, use $scope.gameContainer.cellListArray instead of cellList.
+  // Everywhere else in your program, use $scope.gameContainer.clickCounter instead of clickCount.
+  // Make that change in your ng-repeat as well and anywhere in your index.html as needed.
+
+
+  // remoteGameContainer: that is the name you gave the Firebase node (looks like a folder in Firebase).
+  // The bind statement creates a connection between anything in your app and the Firebase connection we just created.
+   
+  $scope.remoteGameContainer.$bind($scope, "gameContainer") ;
+
+ // The bind statement will automatically update your model, in this case cellList, whenever it 
+  // changes on Firebase.  But this will not trigger an Angular update of the interface (index.html)
+  // - we've been relying on the ng-click to wake up Angular and get the gameboard refreshed.
+  // So we put a watch on cellList - this tells Angular to refresh the interface elements, ie ng-class,
+  // whenever the model, in this case celList, changes.
+  
+
+  $scope.$watch('gameContainer', function() {
+    console.log('gameContainer changed!') ;
+  }) ;
+
+    // gameover = false;
+
+    $scope.playerPicks = function(cellObject) {
+    $scope.moveCounter = $scope.moveCounter + 1 ;
+        console.log("Cell was: " + cellObject.status) ;
+        if (($scope.moveCounter % 2) == 1) {
+            cellObject.status = "X" ; 
         } else {
-            cell.status = "O" ;
+            cellObject.status = "O" ;
         } 
-        console.log("Cell is now: " + cell.status) ;
-        // if (gameover == false) evaluateWin();
-    
+        console.log("Cell is now: " + cellObject.status) ;
+
+
+           // The four lines below are a test to make sure that the win logic is registering properly.
+//         console.log("1st cell status-"+$scope.gameContainer.gameBoardArray[0].status);
+//         console.log("2st cell status-"+$scope.gameContainer.gameBoardArray[1].status);
+//         console.log("3st cell status-"+$scope.gameContainer.gameBoardArray[2].status);
+// console.log("1st cond"+($scope.gameContainer.gameBoardArray[0].status == $scope.gameContainer.gameBoardArray[1].status) && ($scope.gameContainer.gameBoardArray[1].status == $scope.gameContainer.gameBoardArray[2].status));
+
+    if (($scope.gameContainer.gameBoardArray[0].status == $scope.gameContainer.gameBoardArray[1].status) && ($scope.gameContainer.gameBoardArray[1].status == $scope.gameContainer.gameBoardArray[2].status) ||
+    ($scope.gameContainer.gameBoardArray[3].status == $scope.gameContainer.gameBoardArray[4].status) && ($scope.gameContainer.gameBoardArray[4].status == $scope.gameContainer.gameBoardArray[5].status) ||
+    ($scope.gameContainer.gameBoardArray[6].status == $scope.gameContainer.gameBoardArray[7].status) && ($scope.gameContainer.gameBoardArray[7].status == $scope.gameContainer.gameBoardArray[8].status) ||
+    ($scope.gameContainer.gameBoardArray[0].status == $scope.gameContainer.gameBoardArray[3].status) && ($scope.gameContainer.gameBoardArray[3].status == $scope.gameContainer.gameBoardArray[6].status) ||
+    ($scope.gameContainer.gameBoardArray[1].status == $scope.gameContainer.gameBoardArray[4].status) && ($scope.gameContainer.gameBoardArray[4].status == $scope.gameContainer.gameBoardArray[7].status) ||
+    ($scope.gameContainer.gameBoardArray[2].status == $scope.gameContainer.gameBoardArray[5].status) && ($scope.gameContainer.gameBoardArray[5].status == $scope.gameContainer.gameBoardArray[8].status) ||
+    ($scope.gameContainer.gameBoardArray[0].status == $scope.gameContainer.gameBoardArray[4].status) && ($scope.gameContainer.gameBoardArray[4].status == $scope.gameContainer.gameBoardArray[8].status) ||
+    ($scope.gameContainer.gameBoardArray[2].status == $scope.gameContainer.gameBoardArray[4].status) && ($scope.gameContainer.gameBoardArray[4].status == $scope.gameContainer.gameBoardArray[6].status)) 
+    {
+    console.log("X wins!");
+} else if ( $scope.moveCounter == 9) {
+    console.log("Tie game!");
+} else {
+   "" ;
+}
+
 
     
-
-
-    var evaluateWin = function() {
-        if ($scope.gameBoard[0] == "X" && $scope.gameBoard[1] == "X" && $scope.gameBoard[2] == "X") {
-            console.log("x");
-        } else if ($scope.gameBoard[3] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[5] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[6] == "X" && $scope.gameBoard[7] == "X" && $scope.gameBoard[8] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[0] == "X" && $scope.gameBoard[3] == "X" && $scope.gameBoard[6] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[1] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[7] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[2] == "X" && $scope.gameBoard[5] == "X" && $scope.gameBoard[8] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[0] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[8] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[2] == "X" && $scope.gameBoard[4] == "X" && $scope.gameBoard[6] == "X") {
-            Xwin();
-        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[1] == "O" && $scope.gameBoard[2] == "O") {
-            owin();
-        } else if ($scope.gameBoard[3] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[5] == "O") {
-            owin();
-        } else if ($scope.gameBoard[6] == "O" && $scope.gameBoard[7] == "O" && $scope.gameBoard[8] == "O") {
-            owin();
-        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[3] == "O" && $scope.gameBoard[6] == "O") {
-            owin();
-        } else if ($scope.gameBoard[1] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[7] == "O") {
-            owin();
-        } else if ($scope.gameBoard[2] == "O" && $scope.gameBoard[5] == "O" && $scope.gameBoard[8] == "O") {
-            owin();
-        } else if ($scope.gameBoard[0] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[8] == "O") {
-            owin();
-        } else if ($scope.gameBoard[2] == "O" && $scope.gameBoard[4] == "O" && $scope.gameBoard[6] == "O") {
-            owin();
-        } else if (movecounter == 9) {
-    //         var messageboX = document.getElementById('message');
-    //         $scope.leftMessage = "draw...";
-    //         $scope.rightMessage = "draw...";
-               console.log("The game is a tie");
-        } else {
-            movecounter += 1;
-        }
-    }
     };
     
 
